4a568cb3b6c0f5c76008b50120958250618d9ed8
Michael Regpala <mreg96@yahoo.com>
2021-01-03 14:24:15 -0800

    Main prograim commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,59 @@
 #Created By Michael Regpala
-#Created on 2021-01-21
+#Created on 2021-01-02
 #Purpose Homework assignment to track revenue
 
 #Modified
+
+import os
+import csv
+
+#Declare infile and outfile path
+budgetcsv = os.path.join(".","Resources","budget_data.csv")
+summarytxt = os.path.join(".","analysis","budget_summary.txt")
+
+totalmonths = 0
+totalprofitloss = 0 
+priormonthprofitloss = 0
+totalprofitchange = 0
+
+#Decoare list. plschange will be a list of tuples.  
+plchange = []
+
+with open(budgetcsv) as infile:
+    csvreader = csv.reader(infile)
+    header = next(csvreader,None)
+    for row in csvreader:
+      currentprofitloss = float(row[header.index('Profit/Losses')])
+      month = row[header.index('Date')]
+      if totalmonths == 0:
+          #First month will not have a change
+          priorprofitloss = currentprofitloss
+      else:
+          profitlosschange = currentprofitloss - priorprofitloss
+          plchange.append((profitlosschange,month))
+          totalprofitchange = totalprofitchange + profitlosschange
+          priorprofitloss = currentprofitloss
+      totalmonths = totalmonths + 1
+      totalprofitloss = totalprofitloss + float(row[header.index('Profit/Losses')])
+
+
+plchange.sort(reverse=True)
+averagechange = totalprofitchange / (totalmonths - 1)
+#Print to terminal
+print(f'Total Months: {totalmonths}')
+print('Total Profit: $%1.0f' %totalprofitloss)
+print('Average Change: $%.2f' %averagechange )
+print(f'Greatest Increase in Profits: {plchange[0][1]} (${int(plchange[0][0])})')
+print(f'Greatest Decrease in Profits: {plchange[-1][1]} (${int(plchange[-1][0])})')
+
+#Print to file
+with open(summarytxt,"w") as outfile:
+    outfile.write(f'Total Months: {totalmonths}\n')
+    outfile.write('Total Profit: $%1.0f\n' %totalprofitloss)
+    outfile.write('Average Change: $%.2f\n' %averagechange )
+    outfile.write(f'Greatest Increase in Profits: {plchange[0][1]} (${int(plchange[0][0])})\n')
+    outfile.write(f'Greatest Decrease in Profits: {plchange[-1][1]} (${int(plchange[-1][0])})\n')
+
+
+
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,55 @@
 #Created By Michael Regpala
-#Created on 2021-01-21
+#Created on 2021-01-03
 #Purpose Homework assignment to summarize election results
 
 #Modified
+import os
+import csv
+
+#Declare infile and outfile path
+csvfile = os.path.join(".","Resources","election_data.csv")
+summarytxt = os.path.join(".","analysis","election_summary.txt")
+
+#Initialize dictionary
+electdict = dict()
+
+totalvotes = 0
+
+with open(csvfile) as infile:
+    csvreader = csv.reader(infile)
+    header = next(csvreader,None)
+    for row in csvreader:
+        candidate = row[header.index('Candidate')]
+        if len(electdict) == 0:
+            #electdict[row[header.index('Candidate')]]
+            electdict[candidate] = 1
+        else:
+            if candidate not in electdict:
+                electdict[candidate] = 1
+            else:
+                electdict[candidate] = electdict[candidate] + 1
+        totalvotes = totalvotes + 1
+
+sorteddict = sorted(electdict.items(), key=lambda x: x[1], reverse=True)
+print("Election Results") 
+print("-------------------------")  
+print(f"Total Votes {totalvotes}") 
+print("-------------------------") 
+for i in sorteddict:
+    pctvotes = round((i[1]/totalvotes) * 100,3)
+    print(f'{i[0]}: {pctvotes}% ({i[1]})')             
+print("-------------------------") 
+print(f'Winner: {sorteddict[0][0]}')
+print("-------------------------")  
+
+with open(summarytxt,"w") as outfile:
+    outfile.write("Election Results\n")
+    outfile.write("-------------------------\n")
+    outfile.write(f"Total Votes {totalvotes}\n")
+    outfile.write("-------------------------\n") 
+    for i in sorteddict:
+        pctvotes = round((i[1]/totalvotes) * 100,3)
+        outfile.write(f'{i[0]}: {pctvotes}% ({i[1]})\n') 
+    outfile.write("-------------------------\n")    
+    outfile.write(f'Winner: {sorteddict[0][0]}\n')
+    outfile.write("-------------------------")    
